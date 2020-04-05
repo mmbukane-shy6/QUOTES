60b58a114a52fff936deadb2ad04a61ce5800064
mmbukaneshy6 <sheilaegeidza@gmail.com>
2020-04-05 11:02:21 +0300

    all changes

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -1,3 +1,25 @@
+// import { Pipe, PipeTransform } from '@angular/core';
+
+// @Pipe({
+//   name: 'dateCount'
+// })
+// export class DateCountPipe implements PipeTransform {
+
+//   transform(value: any): number {
+//     let today:Date = new Date(); //get current date and time
+//     let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
+//     var dateDifference = Math.abs(value - todayWithNoTime) //returns value in days
+//     const secondsInDay = 86400; //60 seconds * 60 minutes in an hour * 24 hours in a day
+//     var dateDifferenceSeconds = dateDifference*0.001; //converts miliseconds to seconds
+//     var dateCounter = dateDifferenceSeconds/secondsInDay;
+
+//     if (dateCounter >= 1 && value > todayWithNoTime){
+//       return dateCounter;
+//     }else{
+//       return 0;
+//     }
+//   }
+// }
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
@@ -6,17 +28,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DateCountPipe implements PipeTransform {
 
   transform(value: any): number {
-    let today:Date = new Date(); //get current date and time
-    let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
-    var dateDifference = Math.abs(value - todayWithNoTime) //returns value in days
-    const secondsInDay = 86400; //60 seconds * 60 minutes in an hour * 24 hours in a day
-    var dateDifferenceSeconds = dateDifference*0.001; //converts miliseconds to seconds
-    var dateCounter = dateDifferenceSeconds/secondsInDay;
-
-    if (dateCounter >= 1 && value > todayWithNoTime){
+    let today:Date = new Date();//gets current date
+    let todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate());
+    //gets userinput date
+    var dateDifference = Math.abs(todayWithNoTime - value);
+    const secondsInDay = 86400;
+    var dateDifferenceSeconds = dateDifference*0.001;
+    var dateCounter = Math.floor(dateDifferenceSeconds/secondsInDay);
+    if(dateCounter >= 1 && value < todayWithNoTime){
       return dateCounter;
-    }else{
+    } else {
       return 0;
     }
   }
+
 }
